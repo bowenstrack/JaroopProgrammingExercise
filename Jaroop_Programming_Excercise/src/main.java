17073da9ebe378f669a214158b4d278a5d642abb
Bowen Strack <bowenstrack@gmail.com>
2018-04-17 10:38:28 -0400

    Update main.java

--- Jaroop_Programming_Excercise/src/main.java
+++ Jaroop_Programming_Excercise/src/main.java
@@ -3,9 +3,3 @@ public class main {
         atm.run();
     }
 }
-/**
- * My Approach:
- * When I first started working on this problem, I immediately thought of a class I took much earlier in my college career,
- * my Introduction to Object Oriented Programming class. We m
- *
- */
